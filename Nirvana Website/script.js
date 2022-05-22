88d4d0456f206eb8a4080aad04087da860158fd2
OriNinja <95003779+OriNinja@users.noreply.github.com>
2022-05-21 21:44:08 -0700

    Delete Nirvana Website directory
    gonna re-add these

--- Nirvana Website/script.js
+++
@@ -1,7 +0,0 @@
-var mobile = document.getElementById("mobile-view")
-var desktop = document.getElementById("desktop-view")
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  desktop.style = "display: none"
-}else{
-  mobile.style = "display: none"
-}
